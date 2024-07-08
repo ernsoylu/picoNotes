2032ac2cd1c8df87851c48b55c55f4e17f1e7561
Eren Soylu <ernsoylu@gmail.com>
2024-07-08 12:41:19 +0100

    basicCommands project created.

---
+++ basicCommands/build/elf2uf2/tmp/ELF2UF2Build-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/erensoylu/.pico-sdk/sdk/1.5.1/tools/elf2uf2"
+  "/Users/erensoylu/basicCommands/build/elf2uf2"
+  "/Users/erensoylu/basicCommands/build/elf2uf2"
+  "/Users/erensoylu/basicCommands/build/elf2uf2/tmp"
+  "/Users/erensoylu/basicCommands/build/elf2uf2/src/ELF2UF2Build-stamp"
+  "/Users/erensoylu/basicCommands/build/elf2uf2/src"
+  "/Users/erensoylu/basicCommands/build/elf2uf2/src/ELF2UF2Build-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/erensoylu/basicCommands/build/elf2uf2/src/ELF2UF2Build-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/Users/erensoylu/basicCommands/build/elf2uf2/src/ELF2UF2Build-stamp${cfgdir}") # cfgdir has leading slash
+endif()
