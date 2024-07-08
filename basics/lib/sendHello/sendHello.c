c3558fb8c8af00aa245045e8a40d61d5e38a088d
Eren Soylu <ernsoylu@gmail.com>
2024-07-08 13:51:32 +0100

    basics project created.

---
+++ basics/lib/sendHello/sendHello.c
@@ -0,0 +1,6 @@
+#include "sendHello.h"
+#include <stdio.h>
+
+extern void sendHello(void){
+     printf("Hello, world!\n");
+}
--- basics/lib/sendMessage/sendMessage.c
+++
@@ -1,6 +0,0 @@
-#include "sendMessage.h"
-#include <string.h>
-extern char sendMessage(char c[]){
-    strcat(c, " message send by library function.");
-    return c;
-}
